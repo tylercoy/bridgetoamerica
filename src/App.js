287b15302693854490212b4bb4258388400d0fbb
Tyler Coy <tcoy001@fiu.edu>
2024-09-29 13:13:38 -0400

    modified the background image, answer box, input, and button.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Route, Routes, useNavigate } from "react-router-dom"; // Import useNavi
 
 // Components
 import DisplayImageWithName from './Components/displayImageWithName';
-import Navbar from './Components/Navbar'; 
+import Navbar from './Components/Navbar/Navbar'; 
 import Welcome from './pages/Welcome';
 import Home from './pages/home';
 import About from './pages/About';
@@ -16,7 +16,7 @@ import React from 'react';
 
 function App() {
   const [question, setQuestion] = useState("");
-  const [answer, setAnswer] = useState(""); 
+  const [answer, setAnswer] = useState(null); 
   const navigate = useNavigate(); // Initialize useNavigate hook
 
   async function generateAnswer() {
@@ -30,6 +30,7 @@ function App() {
     });
     
     setAnswer(response.data.candidates[0].content.parts[0].text);
+    setQuestion('')
   }
 
   const goToHome = () => {
@@ -39,25 +40,44 @@ function App() {
   return (
     <div className="App">
       <Navbar /> {/* Place Navbar at the top */}
-      <h1 className='bg-green-500'>BridgeToAmerica ChatBot</h1>
+      <div 
+        className='bta-container'
+        style={{
+        backgroundImage: `url(${image})`,
+        
+      }}>
+        <h1 className='bg-green-500'>BridgeToAmerica ChatBot</h1>
 
-      <div style={{ position: 'relative' }}> {/* Relative positioning for the image and chatbot */}
-        <DisplayImageWithName image={image} />
-        <div className="chatbot">
-          <p>Your message here</p>
-          {/* Add your input field or buttons here */}
+        <div style={{ position: 'relative' }}> {/* Relative positioning for the image and chatbot */}
+          {/* <DisplayImageWithName image={image} /> */}
+          <div className="chatbot">
+            {
+              !answer ? (
+                <p>Your message here</p>
+              ) : (
+                answer
+              )
+            }
+            {/* Add your input field or buttons here */}
+          </div>
+        </div>
+
+        <div 
+          className='input-button-container'
+        >
+          <div>
+            <input
+              value={question}
+              onChange={(e) => setQuestion(e.target.value)}
+              cols="5"
+              rows="5"
+              placeholder='Lets do this together! Ask me to start learning journey.'
+            ></input>
+          </div>
+          <button 
+            onClick={generateAnswer}>Generate Answer</button>
         </div>
       </div>
-
-      <textarea
-        value={question}
-        onChange={(e) => setQuestion(e.target.value)}
-        cols="5"
-        rows="5"
-        placeholder='Lets do this together! Ask me to start learning journey.'
-      ></textarea>
-      <button onClick={generateAnswer}>Generate Answer</button>
-      <p>{answer}</p>
 
       <div className="container">
         <Routes>
@@ -66,19 +86,21 @@ function App() {
           <Route path="placeholder" element={<Welcome />} />
         </Routes>
       </div>
+      <div>
+        <a href="https://latino.si.edu/learn/teaching-and-learning-resources">
+          Educational Resources
+        </a>
+        <a href="https://www.youtube.com/playlist?list=PLYCXLjOiLD_A1Z6rR4KFQ7bhMPjO2yDF4">
+          Start Your Journey
+        </a>
+        <a href="https://es.khanacademy.org/?_gl=1*xvryn2*_ga*MTk1NTQ2MTQ0NS4xNzI3NjE5NDM0*_ga_19G17DJYEE*MTcyNzYxOTQzNC4xLjAuMTcyNzYxOTQzNC4wLjAuMA..">
+          Learn With Us
+        </a>
 
-      <a href="https://latino.si.edu/learn/teaching-and-learning-resources">
-        <button>Educational Resources</button>
-      </a>
-      <a href="https://www.youtube.com/playlist?list=PLYCXLjOiLD_A1Z6rR4KFQ7bhMPjO2yDF4">
-        <button>Start Your Journey</button>
-      </a>
-      <a href="https://es.khanacademy.org/?_gl=1*xvryn2*_ga*MTk1NTQ2MTQ0NS4xNzI3NjE5NDM0*_ga_19G17DJYEE*MTcyNzYxOTQzNC4xLjAuMTcyNzYxOTQzNC4wLjAuMA..">
-        <button>Learn With Us</button>
-      </a>
+      </div>
 
       {/* Button to navigate to the Home page */}
-      <button onClick={goToHome}>Go to Home</button>
+      
     </div>
   );
 }
